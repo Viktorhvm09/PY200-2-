91ca81b9589eadfc4fedba76adadf1050ddebcc3
Viktorhvm09 <hvm09@rambler.ru>
2025-02-01 01:14:32 +0300

    2

--- Занятие 1. Основы ООП/1. Практические задания/task1_Glass__init__/task.py
+++ Занятие 1. Основы ООП/1. Практические задания/task1_Glass__init__/task.py
@@ -10,13 +10,29 @@ class Glass:
         """
 
         # TODO создайте атрибут capacity_volume и occupied_volume Обязательно проверяйте типы (TypeError) и значения передаваемых аргументов (ValueError)
+        if not isinstance(capacity_volume, (int, float)):
+            raise TypeError("Должно быть int или float")
+        if capacity_volume <= 0:
+            raise ValueError("Должно быть больше 0")
+        self.capacity_volume = capacity_volume
 
+        if not isinstance(occupied_volume, (int, float)):
+            raise TypeError("Должно быть int или float")
+        if occupied_volume < 0:
+            raise ValueError("Не может быть меньше 0")
+        if occupied_volume > capacity_volume:
+            raise ValueError("Заполнение превышает объем стакана")
+        self.occupied_volume = occupied_volume
 
 if __name__ == "__main__":
     # TODO инициализировать два объекта от класса Стакан (Glass)
+    glass1 = Glass(100, 50)
+    glass2 = Glass(200, 100)
+
 
     try:
         ...  # TODO инициализировать не корректные объекты
+        glass3 = Glass(100, 150)
     except Exception as err:
         print(f"Была вызвана ошибка {err!r}")
     else:
